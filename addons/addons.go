058f0300151e74cc88324ed5c33cdb7299f69ced
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-02-12 16:26:09 +0000

    Add files via upload

---
+++ addons/addons.go
@@ -0,0 +1,23 @@
+package addons
+
+//You can add whatever you want here to interpret the input of the user. I've included an example
+
+//simple reverse a string function
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Parse(usertext string) (output string) {
+	output = usertext
+	if len(usertext) > 4 {
+		switch usertext[0:3] {
+		case "rev":
+			output = reverse(usertext[4:]) //if text starts with 'rev' then you reverse the rest of the string
+		}
+	}
+	return output
+}
